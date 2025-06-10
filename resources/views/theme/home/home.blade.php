a0ed37866ead49ca139c96dd80a44b72fabe9fcb
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-06-10 06:20:29 -0500

    chnages can be done

--- resources/views/theme/home/home.blade.php
+++ resources/views/theme/home/home.blade.php
@@ -131,6 +131,38 @@
 
     });
 
+    $(document).ready(function(){
+        $('.brand-slider').slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: true,
+            dots: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                }
+            ]
+        });
+    });
+
 </script>
 
 
