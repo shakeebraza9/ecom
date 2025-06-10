05d122ecf6651325f9af11b2032d0443d41eee4b
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-06-10 06:49:02 -0500

    chnages can be done

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -37,7 +37,7 @@ class ProductController extends Controller
     |
     */
 
- 
+
     /**
      * Create a new controller instance.
      *
@@ -53,7 +53,7 @@ class ProductController extends Controller
 
             $query = Product::Query();
 
-    
+
             $search = $request->get('search')['value'];
             if($search != ""){
                $query = $query->where(function ($s) use($search) {
@@ -82,7 +82,7 @@ class ProductController extends Controller
             $records = $query->skip($request->start)
             ->take($request->length)->orderBy('id','desc')
             ->get();
-            
+
             $data = [];
             foreach ($records as $key => $value) {
 
@@ -96,7 +96,7 @@ class ProductController extends Controller
 
                 $action .= '</div>';
 
-                $thumb_path = $value->image ? $value->image : ''; 
+                $thumb_path = $value->image ? $value->image : '';
                 $img = '<img  style="width:100px;height:50px" src="'.asset($thumb_path).'" />';
 
                 array_push($data,[
@@ -121,15 +121,15 @@ class ProductController extends Controller
                 'data'=> $data,
             ]);
         }
-        
+
 
         $categories = Category::all();
-        
-        
+
+
         return view('admin.products.index',compact('categories','categoryDropdown'));
     }
 
-    
+
 
 
     /**
@@ -172,10 +172,10 @@ class ProductController extends Controller
             'is_enable' => 0,
         ]);
 
-        return redirect('/admin/products/edit/'.Crypt::encryptString($product->id))->with('success','Record Created Success'); 
+        return redirect('/admin/products/edit/'.Crypt::encryptString($product->id))->with('success','Record Created Success');
     }
 
-   
+
 
      /**
      * Create a new controller instance.
@@ -186,36 +186,36 @@ class ProductController extends Controller
     {
         $id = Crypt::decryptString($id);
         $product = Product::find($id);
-        if (!$product) {  
+        if (!$product) {
             return back()->with('error', 'Record Not Found');
         }
-    
+
         // Convert images string to an array
         $productImages = explode(',', $product->images);
-    
+
         $categories = Category::all();
         $brands = Brand::all();
         $attributes = Attribute::with('values')->get();
         $collections = Collection::where('is_enable', 1)->get();
-    
+
         return view('admin.products.edit', compact('product', 'productImages', 'categories', 'brands', 'attributes', 'collections'));
     }
 
     function generateAttributeCombinations($attributes) {
         $result = [[]]; // Initialize with an empty combination
-    
+
         foreach ($attributes as $attribute) {
             $currentResult = [];
-    
+
             foreach ($result as $item) {
                 foreach ($attribute['values'] as $value) {
                     $currentResult[] = array_merge($item, [ $value]);
                 }
             }
-    
+
             $result = $currentResult;
         }
-    
+
         return $result;
     }
 
@@ -230,7 +230,7 @@ class ProductController extends Controller
 
     //     // dd($request->all());
 
-        
+
     //     $id = Crypt::decryptString($id);
     //     $validator = Validator::make($request->all(), [
     //         "title" => 'required|max:255',
@@ -254,7 +254,7 @@ class ProductController extends Controller
     //     }
 
     //     $product = Product::find($id);
-    //     if($product == false){  
+    //     if($product == false){
     //        return back()->with('error','Record Not Found');
     //     }
 
@@ -262,7 +262,7 @@ class ProductController extends Controller
     //     ProductCollection::where('product_id',$product->id)->delete();
     //     if($request->has('collections')){
     //         foreach ($request->collections as $collect) {
-                
+
     //             ProductCollection::create([
     //                 'product_id' =>    $product->id,
     //                 'collection_id' => $collect,
@@ -294,7 +294,7 @@ class ProductController extends Controller
     //     $product->selling_price = $request->selling_price;
     //     $product->price = $request->price;
 
-    //     $product->sku = $request->sku;   
+    //     $product->sku = $request->sku;
     //     $product->tags = $request->tags;
     //     $product->save();
 
@@ -305,7 +305,7 @@ class ProductController extends Controller
     public function update(Request $request, $id)
     {
         $id = Crypt::decryptString($id); // Decrypt the ID if encrypted
-        
+
         $validator = Validator::make($request->all(), [
             'title' => 'required|max:255',
             'slug' => [
@@ -316,26 +316,28 @@ class ProductController extends Controller
             'details' => 'max:500',
             'description' => 'max:9000',
             'category_id' => 'integer',
+            'brand_id' => 'integer',
             'meta_title' => 'max:255',
             'meta_description' => 'max:255',
             'meta_keywords' => 'max:255',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
-    
+
         $product = Product::find($id);
         if (!$product) {
             return response()->json(['error' => 'Record not found'], 404);
         }
-    
+
         // Update product fields
         $product->title = $request->title;
         $product->slug = $request->slug;
         $product->details = $request->details;
         $product->description = $request->description;
         $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
         $product->meta_title = $request->meta_title;
         $product->meta_description = $request->meta_description;
         $product->meta_keywords = $request->meta_keywords;
@@ -346,7 +348,7 @@ class ProductController extends Controller
 
         $product->image = $request->image;
         $product->hover_image = $request->hover_image;
-    
+
         // Handle collections
         ProductCollection::where('product_id', $product->id)->delete();
         if ($request->has('collections')) {
@@ -357,14 +359,14 @@ class ProductController extends Controller
                 ]);
             }
         }
-    
+
         // Handle gallery
         if ($request->has('gallery')) {
             $product->images = implode(',', $request->gallery);
         }
-    
+
         $product->save();
-    
+
         return response()->json(['success' => 'Record updated successfully'], 200);
     }
      /**
@@ -381,12 +383,12 @@ class ProductController extends Controller
         }else{
             ProductCollection::where('product_id',$product->id)->delete();
             $product->delete();
-            return redirect('/admin/products/index')->with('success','Record Deleted Success'); 
+            return redirect('/admin/products/index')->with('success','Record Deleted Success');
         }
 
     }
 
-    
+
 
         /**
      * Create a new controller instance.
@@ -397,19 +399,19 @@ class ProductController extends Controller
     {
         $id = Crypt::decryptString($id);
         $product = Product::find($id);
-        if($product == false){  
+        if($product == false){
            return back()->with('error','Record Not Found');
         }
 
-         $images = explode(',',$product->images); 
+         $images = explode(',',$product->images);
          $array_without_strawberries = array_diff($images, array($request->image));
          $product->images = implode(',',$array_without_strawberries);
          $product->save();
-        return back()->with('success','Record Removed Success'); 
+        return back()->with('success','Record Removed Success');
 
     }
 
-    
+
 
     /**
      * Create a new controller instance.
@@ -419,28 +421,28 @@ class ProductController extends Controller
     {
         $id = Crypt::decryptString($id);
         $product = Product::find($id);
-    
+
         // Delete existing variations
         Variation::where('product_id', $id)->delete();
-    
+
         // Generate new attribute combinations
         $values = $product->generateAttributeCombinations($request->attr);
-    
+
         $variations = [];
-    
+
         foreach ($values as $combination) {
             $sku = [];
             foreach ($combination as $item) {
                 array_push($sku, $item['title']);
             }
-    
+
             $ProductVariation = Variation::create([
                 "product_id" => $id,
                 "title" => implode('-', $sku),
                 "sku" => implode('-', $sku),
                 "value" => implode('-', $sku)
             ]);
-    
+
             foreach ($combination as $item) {
                 VariationAttribute::create([
                     "variation_id" => $ProductVariation->id,
@@ -449,17 +451,17 @@ class ProductController extends Controller
                     "value" => $item['title'],
                 ]);
             }
-    
+
             $variations[] = $ProductVariation->load('attributes');
         }
-    
+
         // Return the newly created variations as JSON
         return response()->json([
             'success' => true,
             'message' => 'Variations Generated Successfully',
         ]);
     }
-    
+
 
 
     /**
@@ -486,13 +488,13 @@ class ProductController extends Controller
         }
     }
     public function getvariations(Request $request, $id)
-    
+
     {
         $id = Crypt::decryptString($id);
-       
-      
+
+
         $variations = Variation::where('product_id', $id)->with('attributes')->get();
-    
+
         if ($variations->isNotEmpty()) {
             return response()->json([
                 'success' => true,
@@ -504,13 +506,13 @@ class ProductController extends Controller
             return response()->json(['success' => false, 'message' => 'Variations not found.']);
         }
     }
-    
+
     public function variationsUpdate(Request $request,$id){
         $variations = $request->input('variations');
 
         foreach ($variations as $variationData) {
             $variation = Variation::find($variationData['id']);
-            
+
             if ($variation) {
                 $variation->quantity = $variationData['quantity'];
                 $variation->price = $variationData['price'];
@@ -518,9 +520,9 @@ class ProductController extends Controller
                 $variation->save();
             }
         }
-    
+
         return response()->json(['message' => 'Variations updated successfully']);
 
-       
+
     }
 }
